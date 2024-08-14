c8f28671669a3ea01a195e0c76e8d037042e9cf1
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-14 11:17:23 +0100

    chore: config settings

--- src/types/plugin-inputs.ts
+++ src/types/plugin-inputs.ts
@@ -11,18 +11,50 @@ export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU 
   ref: string;
 }
 
-/**
- * This should contain the properties of the bot config
- * that are required for the plugin to function.
- *
- * The kernel will extract those and pass them to the plugin,
- * which are built into the context object from setup().
- */
 export const pluginSettingsSchema = T.Object(
   {
-    configurableResponse: T.String(),
+    /**
+     * The private key of the EOA which holds the funds and
+     * acts as a "faucet". This wallet should be only be used
+     * for this purpose and should be protected.
+     * 
+     * TODO: Confirm if it needs encrypted
+     */
+    fundingWalletPrivateKey: T.String({ pattern: "^0x[a-fA-F0-9]{64}$" }),
+    /**
+     * The networkIds that the faucet should support. Meaning any network
+     * listed here should contain enough funds for any specified token, ERC20 or native
+     * at the `fundingWalletPrivateKey` address on that network.
+     */
+    networkIds: T.Array(T.Number(), { minItems: 1 }),
+    /**
+     * If this is defined then the faucet will only distribute the native gas token
+     * (e.g. ETH, MATIC, XDAI, etc...) to the recipients.
+     */
+    nativeGasToken: T.Optional(T.BigInt({ minimum: BigInt(0) })),
+    /**
+     * If `nativeGasToken` is not defined then these are the tokens that will be distributed
+     * based on slash command usage.
+     * 
+     * An optional array containing the token addresses and amounts that are
+     * to be distributed to the recipients.
+     */
+    distributionTokens: T.Optional(T.Array(T.Object({
+      /**
+       * The amount of tokens to distribute to each recipient
+       * in wei. 
+       * 
+       * Native gas tokens === 1e18 (1e18 wei = 1 ether = 1 Eth = 1 XDAI = 1 MATIC etc...)
+       * 
+       * https://eth-converter.com/
+       */
+      amount: T.BigInt({ minimum: BigInt(0) }),
+      /**
+       * The token address to distribute.
+       */
+      tokenAddress: T.String(),
+    }))),
   },
-  { default: { configurableResponse: "Hello, world!" } }
 );
 
 export const pluginSettingsValidator = new StandardValidator(pluginSettingsSchema);
