11caa68ca68e4baabd251c74efb38852cd88ef0f
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-29 17:44:52 +0100

    chore: remove unused config options and typeguards

--- src/types/plugin-inputs.ts
+++ src/types/plugin-inputs.ts
@@ -1,7 +1,6 @@
 import { SupportedEvents, SupportedEventsU } from "./context";
 import { StaticDecode, Type as T } from "@sinclair/typebox";
 import { StandardValidator } from "typebox-validators";
-import { throwError } from "../utils/logger";
 
 export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU extends SupportedEvents[T] = SupportedEvents[T]> {
   stateId: string;
@@ -15,63 +14,28 @@ export interface PluginInputs<T extends SupportedEventsU = SupportedEventsU, TU 
 export type Args = {
   recipient: string;
   networkId: string;
-  amount?: bigint;
-  token?: string;
+  amount: bigint;
 };
 
 export const pluginSettingsSchema = T.Object({
-  howManyTimesUserCanClaim: T.Number({ minimum: 1, default: 1 }),
   /**
    * The private key of the EOA which holds the funds and
    * acts as a "faucet". This wallet should be only be used
    * for this purpose and should be protected.
-   *
-   * TODO: Confirm if it needs encrypted
    */
   fundingWalletPrivateKey: T.String({ pattern: "^0x[a-fA-F0-9]{64}$", minLength: 66, maxLength: 66 }),
   /**
    * The networkIds that the faucet should support. Meaning any network
-   * listed here should contain enough funds for any specified token, ERC20 or native
-   * at the `fundingWalletPrivateKey` address on that network.
+   * listed here should contain enough funds at the `fundingWalletPrivateKey`
+   * address on that network.
    */
-  networkIds: T.Array(
-    T.Transform(T.Union([T.String(), T.Number()]))
-      .Decode((value) => {
-        if (typeof value === "string") {
-          const networkId = Number(value);
-          if (isNaN(networkId) || networkId <= 0) {
-            throwError(`Invalid networkId: ${value}`);
-          } else {
-            return networkId;
-          }
-        } else {
-          return value;
-        }
-      })
-      .Encode((value) => {
-        return value.toString();
-      })
-  ),
+  networkId: T.Transform(T.Number({ minimum: 1 }))
+    .Decode((v) => v.toString())
+    .Encode((v) => parseInt(v)),
   /**
-   * If this is defined then the faucet will only distribute the native gas token
-   * (e.g. ETH, MATIC, XDAI, etc...) to the recipients.
+   * The native gas token amount to be sent to the user.
    */
-  nativeGasToken: T.Optional(T.BigInt({ minimum: BigInt(0) })),
-  /**
-   * If `nativeGasToken` is not defined then these are the
-   * tokens that will be distributed to the recipients.
-   *
-   * An optional object containing the token address as keys and amounts as values that are
-   * to be distributed to the recipients.
-   *
-   * The amount of tokens to distribute to each recipient
-   * in wei. This is the smallest unit of the token.
-   *
-   * Native gas tokens === 1e18 (1e18 wei = 1 ether = 1 Eth = 1 XDAI = 1 MATIC etc...)
-   *
-   * https://eth-converter.com/
-   */
-  distributionTokens: T.Optional(T.Record(T.String(), T.BigInt({ minimum: BigInt(0) }))),
+  gasSubsidyAmount: T.BigInt({ minimum: BigInt(0) }),
 });
 
 export const pluginSettingsValidator = new StandardValidator(pluginSettingsSchema);
--- src/types/typeguards.ts
+++ src/types/typeguards.ts
@@ -1,13 +1,5 @@
 import { Context } from "./context";
 
-function isCommentEvent(context: Context): context is Context<"issue_comment.created"> {
-  return context.eventName === "issue_comment.created";
-}
-
-export function isIssueCommentEvent(context: Context): context is Context<"issue_comment.created"> {
-  return isCommentEvent(context) && context.payload.comment.body.startsWith("/");
-}
-
 export function isIssueClosedEvent(context: Context): context is Context<"issues.closed"> {
   return context.eventName === "issues.closed";
 }
